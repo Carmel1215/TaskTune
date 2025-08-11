ac68f5ea4ce1673139d26a0f71fffa513245b1d2
Carmel1215 <kth12153@gmail.com>
2025-08-11 20:30:10 +0900

    Feat: light theme 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:tasktune/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final lightScheme = const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color.fromARGB(255, 239, 123, 0), // 북일 대표 색 1
+      onPrimary: Colors.white,
+      secondary: Color.fromARGB(255, 243, 150, 51), // 북일 대표 색 2
+      onSecondary: Colors.white,
+      tertiary: Color.fromARGB(255, 247, 191, 127), // 북일 대표 색 3
+      onTertiary: Colors.black,
+      error: Colors.red,
+      onError: Colors.white,
+      surface: Color.fromARGB(255, 255, 255, 255),
+      onSurface: Colors.black,
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    final lightText = ThemeData(useMaterial3: true).textTheme
+        .apply(); // TODO: apply는 전체 적용, copyWith은 하나의 스타일씩 적용이니까 잘 생각해서 GPT한테 물어봐가면서 스타일 수정하기
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    final darkScheme = const ColorScheme(
+      // TODO: 엉망진창 색깔 고치기
+      brightness: Brightness.dark,
+      primary: Color.fromARGB(255, 255, 111, 0),
+      onPrimary: Colors.white,
+      secondary: Color.fromARGB(255, 255, 111, 0),
+      onSecondary: Colors.white,
+      error: Colors.red,
+      onError: Colors.white,
+      surface: Colors.black,
+      onSurface: Colors.white,
+    );
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    final darkText = ThemeData(useMaterial3: true).textTheme
+        .apply(); // TODO: apply는 전체 적용, copyWith은 하나의 스타일씩 적용이니까 잘 생각해서 GPT한테 물어봐가면서 스타일 수정하기
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: const HomeScreen(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightScheme,
+        textTheme: lightText,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          surfaceTintColor: Colors.transparent,
+          foregroundColor: lightScheme.onPrimary,
+          backgroundColor: lightScheme.primary,
+          shadowColor: Colors.black,
+          elevation: 5,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      darkTheme: ThemeData(
+        colorScheme: darkScheme,
+        textTheme: darkText,
+      ),
+      themeMode: ThemeMode.light,
     );
   }
 }
